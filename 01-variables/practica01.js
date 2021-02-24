8f1bf40003cd7f005a217edff1fd7dc47efda801
pluton <jeanellycomascapdevila@hotmail.com>
2021-02-23 22:32:01 -0500

    tarea 1

---
+++ 01-variables/practica01.js
@@ -0,0 +1,24 @@
+
+// tarea 1
+/* Crear un una carpeta llamada ejercicio y crear un index.html con su instrucciones
+crear un archivo js llamado practica.js y hacer las 4 operaciones básicas  + - * /
+
+*/
+//tarea 2
+/* 
+crear una variable edad con 18 y usar los condicionales si es mayor a 18 sacar un aviso 
+puedes culiar si no no puede culiar
+
+*/
+
+// tarea 3
+/* 
+usando la funcion prompt que es aviso por pantalla 
+pedir la base de un triangualo en otro prompt pedir la altura 
+y calcular el área QUE SE DEBE PINTAR EN LA PÁGINA NO EN LA CONSOLA
+
+
+
+*/
+
+
---
+++ Ejercicio/practica.js
@@ -0,0 +1,20 @@
+document.write('Hola mundo');
+/* 
+2 tipos de concatenación
+console.log(`${resultado}`);
+console.log("El resultado es: "+resulta);
+*/
+let a= 7
+let b=15
+let c=40
+
+let resulta = a+b+c
+
+
+
+
+console.log(a+b+c);
+console.log(a+b+c);
+
+
+
